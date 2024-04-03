a1b459dfe609425692454ae5bf782a76692e6eef
BigOrangeTJ <rpartridge@cocc.edu>
2024-04-03 14:59:37 -0700

    Function Added

---
+++ PartridgeArea.py
@@ -0,0 +1,3 @@
+def rect_area(length, width):
+    """Given length and width, calculate rectangle area. Return the area."""
+    return length * width
---
+++ PartridgeSolidArea.py
@@ -0,0 +1,3 @@
+def rect_solid_area(length, width, height):
+    """Given length, width, and height, calculate solid rectangle area. Return the area."""
+    return length * width * height
